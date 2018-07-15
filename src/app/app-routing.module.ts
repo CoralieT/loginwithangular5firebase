ccf6ba086008be2e5232e40630ae3391c011252e
coralie fogue <coralietsono@gmail.com>
2018-07-15 23:03:07 +0100

    configurer le routeur et l'interface navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './component/home-page/home-page.component';
+import {LoginPageComponent} from './component/login-page/login-page.component';
+import{RegisterPageComponent} from './component/register-page/register-page.component';
+import {PrivatePageComponent} from './component/private-page/private-page.component';
+import {NotFoundPageComponent} from './component/not-found-page/not-found-page.component';
+const routes: Routes = [
 
-const routes: Routes = [];
+  {path:'', component: HomePageComponent},
+  {path:'login', component: LoginPageComponent},
+  {path:'register', component: RegisterPageComponent},
+  {path:'private', component: PrivatePageComponent},
+  {path:'**', component: NotFoundPageComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
