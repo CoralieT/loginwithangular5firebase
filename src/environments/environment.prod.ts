d0f0d96a42701a188c51aa1c63c43baca040baf9
coralie fogue <coralietsono@gmail.com>
2018-07-16 15:17:46 +0100

    config firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyDadhQvQJsezb0Jj8LkaA6NPHvZ6b3guuY",
+    authDomain: "fir-demo-project.firebaseapp.com",
+    databaseURL: "https://fir-demo-project.firebaseio.com",
+    projectId: "fir-demo-project",
+    storageBucket: "fir-demo-project.appspot.com",
+    messagingSenderId: "300830567303"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyDadhQvQJsezb0Jj8LkaA6NPHvZ6b3guuY",
+    authDomain: "fir-demo-project.firebaseapp.com",
+    databaseURL: "https://fir-demo-project.firebaseio.com",
+    projectId: "fir-demo-project",
+    storageBucket: "fir-demo-project.appspot.com",
+    messagingSenderId: "300830567303"
+  }
 };
 
 /*
